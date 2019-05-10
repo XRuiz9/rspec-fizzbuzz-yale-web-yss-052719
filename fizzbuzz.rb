41623137ce2dd965c61fca11fb973f15ae10353d
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-10 21:06:14 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(int)
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
-    "Fizz"    
+    "Fizz"
 end
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
